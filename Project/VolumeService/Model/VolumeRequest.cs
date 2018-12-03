a8cdd208c82a85a470ffaa822c35eb17df7689d4
lberwid <lberwid@sdncenter.pl>
2018-12-03 20:49:57 +0100

    volume service

---
+++ Project/VolumeService/Model/VolumeRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Application.Models;
+
+namespace VolumeService.Model
+{
+    public class VolumeRequest
+    {
+        public IEnumerable<byte[]> Masks { get; set; }
+        public ImageInformation ImageInformation { get; set; }
+    }
+}
