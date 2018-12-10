4f69b46c6ba9eb31f6cb1bc40ee19a08967f7a73
rbuzu <rafal.buzun@gmail.com>
2018-12-10 12:27:27 +0100

    asd

--- viewer/src/containers/PacsImporter.js
+++ viewer/src/containers/PacsImporter.js
@@ -2,7 +2,57 @@ import React, { Component } from "react";
 import { Table } from "react-bootstrap";
 
 export default class PacsImporter extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            patients: []
+        };
+    }
+    componentDidMount() {
+        this.getPatients();
+    }
+
+    getPatientDetails() {
+        const patientId = this.props.match.params.patientId;
+        const that = this;
+        fetch(`http://localhost:8042/patiens/`)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(jsonData => {
+                this.setState({ patients: jsonData });
+
+            })
+    }
+
+    importPatient(patientId) {
+        fetch(`http://localhost:8042/patients/${patientId}/anonymize`)
+        .then(function (response) {
+            console.log(response);
+            return response.json();
+        })
+        .then(jsonData => {
+            fetch(`http://localhost:8042/patients/${patientId}/archive`)
+            .then(function (response) {
+                console.log(response);
+                return response.json();
+            })
+            .then(jsonData => {
+                fetch(`http://localhost:5001/api/NewDicom/Upload`)
+                .then(function (response) {
+                    console.log(response);
+                    return response.json();
+                })
+                .then(jsonData => {
+                    console.log("Patient Uploaded");
+                })
+            })       
+        })
+    }
+
     render() {
+        const listItems = this.state.patients.map((d, index) => <tr><td>index</td><td>d.patient.name</td><td><button onClick={() => this.importPatient(d.id)}className="btn btn-primary">Import</button></td></tr>);
         return (
             <Table striped bordered condensed hover>
                 <thead>
@@ -12,23 +62,9 @@ export default class PacsImporter extends React.Component {
                         <th>Action</th>
                     </tr>
                 </thead>
+                {}
                 <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Prostate3T-03-0023</td>
-                        <td><button className="btn btn-primary">Import</button></td>
-
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Prostate3T-03-0024</td>
-                        <td><button className="btn btn-primary">Import</button></td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>ProstateDx-01-0035</td>
-                        <td><button className="btn btn-primary">Import</button></td>
-                    </tr>
+                    {listItems}
                 </tbody>
             </Table>
         );
